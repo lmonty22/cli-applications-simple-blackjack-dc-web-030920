ed3e055bf49a9d2672d83b18e07d38dcc07b43d9
Lindsay Montgomery <lindsay.montgomery@olympian.org>
2020-02-12 02:16:50 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,53 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
-end
+  rand(1..11)
+end 
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(total)
+  puts "Your cards add up to #{total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
+def end_game(total)
   # code #end_game here
+  puts "Sorry, you hit #{total}. Thanks for playing!"
 end
 
 def initial_round
   # code #initial_round here
-end
-
-def hit?
-  # code hit? here
+  sum = deal_card + deal_card
+  display_card_total(sum)
+  sum
 end
 
 def invalid_command
   # code invalid_command here
+  puts "Please enter a valid command"
+end
+
+def hit?(total)
+  prompt_user
+  input = get_user_input 
+  if input == "h" 
+    total += deal_card
+  end 
+  if input != "s" and input != "h"
+    invalid_command
+    prompt_user
+    get_user_input
+  end
+  total
 end
 
 #####################################################
@@ -39,6 +55,13 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome 
+  sum = initial_round
+  hit?(sum)
+  display_card_total(sum)
+  hit?(sum) until sum > 21 
+  if sum > 21 
+  end_game
+  end
 end
     
